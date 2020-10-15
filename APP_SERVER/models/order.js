a6ed08c0f4e78b977ef3d8ce82e72f36d9e9daa1
kaur08871 <gagandeepkaur28061993@gmail.com>
2020-10-15 00:06:52 -0400

    collection document checkin

---
+++ APP_SERVER/models/order.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var orderdetailsList = new mongoose.Schema({
+    name : {type: String, required: true},
+    quantity: {type: Number, required: true},
+    Menu_item_id:{type : mongoose.Schema.Types.ObjectId, ref : 'Menu'}
+});
+
+var orderSchema = new mongoose.Schema({
+   customer_id:{type : mongoose.Schema.Types.ObjectId, ref : 'Customer'},
+   order_details: [orderdetailsList],
+   amount :{type:Number, required: true},
+   orderdate : {type:Date, required: true},
+   status: {type:String, enum:['Ordered','Delivered','Cancelled']},
+   order_type: {type:String, enum:['Dine-in','Delivery']},
+   special_instructions:{type:String}
+});
+
+
+mongoose.model('Order', orderSchema);
