dca3303887928e092f7f61fb73529d24a111aefb
swetha-mary-philip <smp5993@gmail.com>
2020-10-15 00:09:39 -0400

    Collection document checkin for customer and menu

---
+++ APP_SERVER/models/menu.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var ingredientsList = new mongoose.Schema({
+    name : {type: String, required: [true, 'Ingredient name is required']}
+});
+
+
+var menuSchema = new mongoose.Schema({
+    name : {type: String, required: true, minlength: 3},
+    description: {type: String, required: true, maxlength:1000},
+    imageurl: {type: String, required: true},
+    price: {type: String, required: true},
+    ingredients: [ingredientsList],
+    createdate: {type:Date},
+    modifieddate: {type:Date},
+    avg_rating: {type: Number}
+});
+
+mongoose.model('Menu', menuSchema);
+
