76be35d28c040be302a7341f28f656a6fbdb049c
swetha-mary-philip <smp5993@gmail.com>
2020-10-22 20:35:53 -0400

    add order payment

--- APP_SERVER/models/order.js
+++ APP_SERVER/models/order.js
@@ -6,6 +6,14 @@ var orderdetailsList = new mongoose.Schema({
     Menu_item_id:{type : mongoose.Schema.Types.ObjectId, ref : 'Menu'}
 });
 
+var paymentdetails = new mongoose.Schema({
+
+    cardholdername : {type: String, required: true},
+    cardnumber : {type: Number, required: true},
+    expiry : {type: Number, required: true}, 
+
+})
+
 var orderSchema = new mongoose.Schema({
    customer_id:{type : mongoose.Schema.Types.ObjectId, ref : 'Customer'},
    order_details: [orderdetailsList],
@@ -13,7 +21,8 @@ var orderSchema = new mongoose.Schema({
    orderdate : {type:Date, required: true},
    status: {type:String, enum:['Ordered','Delivered','Cancelled']},
    order_type: {type:String, enum:['Dine-in','Delivery']},
-   special_instructions:{type:String}
+   special_instructions:{type:String},
+   payment : [paymentdetails]
 });
 
 
